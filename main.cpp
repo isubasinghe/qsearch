8156b3f66a8bcad33437bca319ff03028691faaa
isubasinghe <isubasinghe@student.unimelb.edu.au>
2020-07-20 21:03:09 +1000

    removed oatpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,8 @@
 #include "server/server.hpp"
 
 int main(int argc, char *argv[]) {
-    oatpp::base::Environment::init();
 
     server::Server server;
 
-    oatpp::base::Environment::destroy();
     return 0;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,17 +1,9 @@
 #include "server/server.hpp"
 
 namespace server {
-    Server::Server(unsigned short port) {
-        this->serverConnectionProvider = new oatpp::network::server::SimpleTCPConnectionProvider(port);
-    }
     Server::Server() {
-        this->serverConnectionProvider = new oatpp::network::server::SimpleTCPConnectionProvider(DEFAULT_QSEARCH_PORT);
+        
     }
-
-    void Server::build() {
-
-    }
-
     Server::~Server() {
 
     }
